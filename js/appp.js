9119931c37bfd31334d1ea75fb184433093fbff3
Carolina Peraza <carolina.peranie@gmail.com>
2022-12-07 21:02:09 -0600

    CI: se agrega audio y estilos

---
+++ js/appp.js
@@ -0,0 +1,43 @@
+/* variables de las notas, se hizo una variable para cada nota. */
+//Do
+let notaDo = new Audio('assets/notes/crispin1.mp3');
+let notaRe = new Audio('assets/notes/crispin2.mp3');
+let notaMi = new Audio('assets/notes/crispin3.mp3');
+let notaFa = new Audio('assets/notes/crispin4.mp3');
+let notaSol = new Audio('assets/notes/crispin5.mp3');
+let notaLa = new Audio('assets/notes/crispin6.mp3');
+let notaSi = new Audio('assets/notes/crispin7.mp3');
+
+/* llamando a las variables */
+
+//Do
+playBtn1.addEventListener('click', ()=>{
+    notaDo.play()
+});
+//RE
+playBtn2.addEventListener('click', ()=>{
+    notaRe.play()
+}
+);
+//MI
+playBtn3.addEventListener('click', ()=>{
+    notaMi.play()
+});
+//FA
+playBtn4.addEventListener('click', ()=>{
+    notaFa.play()
+});
+//SOL
+playBtn5.addEventListener('click', ()=>{
+    notaSol.play()
+});
+
+//La
+playBtn6.addEventListener('click', ()=>{
+    notaLa.play()
+});
+
+//SI
+playBtn7.addEventListener('click', ()=>{
+    notaSi.play()
+});
